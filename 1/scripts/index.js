76a41398bbde5da4e41b0f973ba3767a45390f4a
7rp9dq0c <7rp9dq0c@users.noreply.github.com>
2026-05-03 02:55:51 +0000

    Deploying to gh-pages from @ wdrx70xn/418063-cat-energy-2@a79eef9d91cb2b68f5f111fbb68ddde6f9d20c74 🚀

---
+++ 1/scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".navigation"),a=document.querySelector(".navigation__toggle");n.classList.remove("navigation--nojs");a.addEventListener("click",()=>{n.classList.contains("navigation--closed")?(n.classList.remove("navigation--closed"),n.classList.add("navigation--opened")):(n.classList.add("navigation--closed"),n.classList.remove("navigation--opened"))});
